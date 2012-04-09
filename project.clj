a004ff883f844cfe39473a5d781bde1fa468f791
David Santiago <david.santiago@gmail.com>
2012-04-09 12:54:36 -0700

    Update project.clj for lein2; remove swank-clojure dev-dep and add a profile for testing with Clojure 1.3.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject pathetic "0.3.0"
   :description "The missing path handling."
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[swank-clojure "1.3.0"]])
+
+  :profiles {:clojure1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
