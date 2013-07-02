06c2518af22eedeb6b0aa441a431846426dd970f
David Santiago <david.santiago@gmail.com>
2013-07-02 16:35:11 -0700

    Have cljx build to /target/generated-src.

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "The missing path handling."
   :dependencies [[org.clojure/clojure "1.5.1"]]
 
-  :source-paths ["generated-src"]
+  :source-paths ["target/generated-src"]
   :plugins [[com.keminglabs/cljx "0.2.2"]]
   :hooks [cljx.hooks]
   :cljx {:builds [{:source-paths ["src"]
-                   :output-path "generated-src"
+                   :output-path "target/generated-src"
                    :rules cljx.rules/clj-rules}
 
                   {:source-paths ["src"]
-                   :output-path "generated-src"
+                   :output-path "target/generated-src"
                    :extension "cljs"
                    :rules cljx.rules/cljs-rules}]}
   :profiles {:clojure1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
