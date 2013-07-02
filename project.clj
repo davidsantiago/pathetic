f2008a2408d72c841023fcce1c3d3d19d63e0122
Julien Eluard <julien.eluard@gmail.com>
2013-07-02 18:42:21 -0300

    Added ClojureScript support.

--- project.clj
+++ project.clj
@@ -1,5 +1,16 @@
-(defproject pathetic "0.4.0"
+(defproject pathetic "0.5.0-SNAPSHOT"
   :description "The missing path handling."
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
 
+  :source-paths ["generated-src"]
+  :plugins [[com.keminglabs/cljx "0.2.2"]]
+  :hooks [cljx.hooks]
+  :cljx {:builds [{:source-paths ["src"]
+                   :output-path "generated-src"
+                   :rules cljx.rules/clj-rules}
+
+                  {:source-paths ["src"]
+                   :output-path "generated-src"
+                   :extension "cljs"
+                   :rules cljx.rules/cljs-rules}]}
   :profiles {:clojure1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}})
