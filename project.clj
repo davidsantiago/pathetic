90edddee3e148f21ac83bdc939a181630605551b
Julien Eluard <julien.eluard@gmail.com>
2013-07-10 19:36:37 -0300

    Simplified project.clj.

--- project.clj
+++ project.clj
@@ -1,34 +1,29 @@
 (defproject pathetic "0.5.0-SNAPSHOT"
   :description "The missing path handling."
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.cemerick/clojurescript.test "0.0.4"]]
 
   :source-paths ["target/generated-src"]
   :test-paths ["target/generated-test"]
   :plugins [[com.keminglabs/cljx "0.3.0"]
             [lein-cljsbuild "0.3.2"]]
-  :hooks [cljx.hook leiningen.cljsbuild]
+  :hooks [cljx.hooks]
   :cljx {:builds [{:source-paths ["src"]
                    :output-path "target/generated-src"
-                   :rules cljx.rules/clj-rules}
+                   :rules :clj}
 
                   {:source-paths ["src"]
                    :output-path "target/generated-src"
-                   :extension "cljs"
-                   :rules cljx.rules/cljs-rules}
+                   :rules :cljs}
 
                   {:source-paths ["test"]
                    :output-path "target/generated-test"
-                   :rules cljx.rules/clj-rules}
+                   :rules :clj}
 
                   {:source-paths ["test"]
                    :output-path "target/generated-test"
-                   :extension "cljs"
-                   :rules cljx.rules/cljs-rules}]}
-  :profiles {:clojure1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :clojure1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :clojure1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :clojure1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+                   :rules :cljs}]}
+  :profiles {:clojure1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :cljsbuild {:builds [{:source-paths ["target/generated-src" "target/generated-test"]
                         :compiler {:output-to "target/cljs/testable.js"
                         :optimizations :whitespace
